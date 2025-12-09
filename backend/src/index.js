db9ee26fbe8e75887e15867ddc822bcdf0c4e24f
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-12-09 09:59:33 +0200

    Attempting to deploy once again

--- backend/src/index.js
+++ backend/src/index.js
@@ -38,10 +38,10 @@ bookingRoutes(app);
 authRoutes(app);
 
 
-app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+app.use(express.static(path.join(__dirname, "../dist")));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../../frontend/dist/index.html"));
+    res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
 
 
