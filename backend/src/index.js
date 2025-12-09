82843a5a07a088636188003014a6eeaef37d5d42
cristian449 <144561600+cristian449@users.noreply.github.com>
2025-12-09 10:20:05 +0200

    Attempting to deploy again using Regex

--- backend/src/index.js
+++ backend/src/index.js
@@ -40,9 +40,10 @@ authRoutes(app);
 
 app.use(express.static(path.join(__dirname, "../dist")));
 
-app.get("*", (req, res) => {
+app.get(/.*/, (req, res) => {
     res.sendFile(path.join(__dirname, "../dist/index.html"));
 });
+
 
 
 
